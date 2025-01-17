392a0377256d5e3f121047f89a0a265d968b8051
jobsonjuniorr <160482109+jobsonjuniorr@users.noreply.github.com>
2025-01-16 21:08:06 -0300

    Second commit implemente  image registration functionality

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path'); // Importando o módulo path
 
 const app = express();
 const PORT = 3000;
@@ -9,9 +10,15 @@ const PORT = 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
+// Configurar o Express para servir arquivos estáticos da pasta 'uploads'
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 // Rotas
 const cadastroRoutes = require('./routes/cadastro');
+const formularioRoutes = require('./routes/formulario'); // Nova rota
+
 app.use('/api/cadastro', cadastroRoutes);
+app.use('/api/formulario', formularioRoutes); // Registrando a nova rota
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
